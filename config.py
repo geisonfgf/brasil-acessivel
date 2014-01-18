362f9b33b3c3e733d96a6fd7c093bc016627c49e
Geison <geisonfgf@gmail.com>
2014-01-18 21:34:27 -0200

    Adding Heroku stuff

--- config.py
+++ config.py
@@ -5,5 +5,8 @@ CSRF_ENABLED = True
 SECRET_KEY = 'brasil5soyz%2-uy0$#f99mfqc0_qa9bx(a0srve)9fwu-@'\
              '@5vkbqyqhacessivel'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
